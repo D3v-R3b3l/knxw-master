1ad9d2e4ff5b89821a1d5a8c17adba4331ea16f8
Travis Capps <fererradani@gmail.com>
2025-12-02 19:11:36 +0000

    File changes

--- src/pages/MarketIntelligence.jsx
+++ src/pages/MarketIntelligence.jsx
@@ -504,7 +504,7 @@ export default function MarketIntelligencePage() {
 
       // Find the new analysis and select it
       // We use the entity fetch to ensure we have full data structure
-      const newAnalysis = await base44.entities.MarketTrend.get(result.data.analysis_id);
+      const newAnalysis = await base44.entities.MarketTrend.get(result.analysis_id);
       setSelectedAnalysis(newAnalysis);
       setTopic("");
       setIndustry("");
